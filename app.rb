84649eaca602843dbdd662b43d5ce582ceb1b68a
Jesse Miner <jesse@givegab.com>
2019-02-24 15:33:52 -0500

    Add songs index route

--- app.rb
+++ app.rb
@@ -3,7 +3,26 @@ require 'sinatra/activerecord'
 Dir['./app/models/**/*.rb'].each { |file| require file }
 
 class App < Sinatra::Base
-  get '/' do
-    { hello: 'world' }.to_json
+  before { content_type :json }
+
+  get '/songs' do
+    Song.select(:id, :file_id, :title, 'a.name artist_name, l.title album_title')
+      .joins('left join artists a on songs.artist_id = a.id')
+      .joins('left join albums l on songs.album_id = l.id')
+      .where.not(file_id: nil)
+      .map { |s| serialize(s) }
+      .to_json
+  end
+
+  private
+
+  def serialize(song)
+    {
+      id: song.id,
+      album: song.album_title,
+      artist: song.artist_name,
+      file_id: song.file_id,
+      title: song.title
+    }
   end
 end
